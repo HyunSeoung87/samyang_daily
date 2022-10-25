b2e3e94d6e9b3256717f4f6a817e170096e1c9e3
hyun <hyun87@mz.co.kr>
2022-10-25 17:01:40 +0900

    프로젝트 세팅

---
+++ src/main/java/hyun/samyang_daily/SamyangDailyApplication.java
@@ -0,0 +1,13 @@
+package hyun.samyang_daily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamyangDailyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SamyangDailyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hyun/samyang_daily/SamyangDailyApplicationTests.java
@@ -0,0 +1,13 @@
+package hyun.samyang_daily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamyangDailyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
