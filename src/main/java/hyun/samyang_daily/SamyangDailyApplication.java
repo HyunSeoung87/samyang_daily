54051c01b08fa2f69c80f69ae85d86892344cf38
hyun <hyun87@mz.co.kr>
2022-10-27 13:21:16 +0900

    CI/CD 세팅

--- src/main/java/hyun/samyang_daily/SamyangDailyApplication.java
+++ src/main/java/hyun/samyang_daily/SamyangDailyApplication.java
@@ -2,12 +2,37 @@ package hyun.samyang_daily;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 public class SamyangDailyApplication {
+    @Configuration
+    @EnableWebSecurity
+    public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+        @Override
+        protected void configure(HttpSecurity http) throws Exception{
+            http
+                    .csrf().disable()
+                    .authorizeRequests()
+                    .antMatchers("/", "/user/availability/**").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                    .formLogin()
+                    .loginPage("/user/login")
+                    .permitAll()
+                    .and()
+                    .logout()
+                    .permitAll();
+        }
+
+    }
     public static void main(String[] args) {
         SpringApplication.run(SamyangDailyApplication.class, args);
+
     }
 
 }
