03d8ba7f9a2072396d0a9e2c9eb3269c2331ec82
hyun <hyun87@mz.co.kr>
2022-10-27 14:07:58 +0900

    Health Check용 페이지 생성

--- src/main/java/hyun/samyang_daily/SamyangDailyApplication.java
+++ src/main/java/hyun/samyang_daily/SamyangDailyApplication.java
@@ -2,10 +2,6 @@ package hyun.samyang_daily;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 public class SamyangDailyApplication {
