fd50ab2b953a117a053a6d3eadd25d95def6f084
hyun <hyun87@mz.co.kr>
2022-10-28 15:05:38 +0900

    TC 추가

--- src/test/java/hyun/samyang_daily/SamyangDailyApplicationTests.java
+++ src/test/java/hyun/samyang_daily/SamyangDailyApplicationTests.java
@@ -1,13 +1,24 @@
 package hyun.samyang_daily;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootTest
 class SamyangDailyApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @RestController
+    public class TestApi {
+
+        @Value("${spring.datasource.username}")
+        private String userName;
+
+        @GetMapping("/test")
+        public String test() {
+            return userName;
+        }
     }
 
 }
